20936497ce5a250920ca08176645d64068f8a80e
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-05 16:21:17 +0530

    Add secondary controller

---
+++ src/main/java/com/iam2kabhishek/painter/SecondaryController.java
@@ -0,0 +1,12 @@
+package com.iam2kabhishek.painter;
+
+import java.io.IOException;
+import javafx.fxml.FXML;
+
+public class SecondaryController {
+
+    @FXML
+    private void switchToPrimary() throws IOException {
+        App.setRoot("primary");
+    }
+}
