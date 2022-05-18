4c28a47ab5e629ebb556d7734557e6a05bc65683
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-19 00:10:37 +0530

    Add module-info

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,7 @@
 module com.iam2kabhishek.painter {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
     opens com.iam2kabhishek.painter to javafx.fxml;
     exports com.iam2kabhishek.painter;
