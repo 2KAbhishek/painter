69a11f98abbdaa13081daed8514ac634ceba3401
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-19 00:15:19 +0530

    Add Painter class

---
+++ src/main/java/com/iam2kabhishek/painter/Painter.java
@@ -0,0 +1,43 @@
+package com.iam2kabhishek.painter;
+
+// import java.awt.image.RenderedImage;
+// import javafx.swing.SwingFXUtils;
+// import javax.imageio.ImageIO;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Stack;
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Cursor;
+import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Button;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Slider;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.ToggleButton;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Ellipse;
+import javafx.scene.shape.Line;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.Shape;
+import javafx.scene.text.Font;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class Painter extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
