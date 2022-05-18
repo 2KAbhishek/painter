86d5fa32c3862b9988e6a335316bd31a76ab9e0b
Abhishek Keshri <iam2kabhishek@gmail.com>
2022-05-19 00:21:14 +0530

    Add HUGE start method, TODO: could use refactoring

--- src/main/java/com/iam2kabhishek/painter/Painter.java
+++ src/main/java/com/iam2kabhishek/painter/Painter.java
@@ -222,6 +222,239 @@ public class Painter extends Application {
         });
     }
 
+    @Override
+    public void start(Stage primaryStage) {
+        Stack<Shape> undoHistory = new Stack();
+        Stack<Shape> redoHistory = new Stack();
+
+        ToggleButton drawBtn = new ToggleButton("Draw");
+        ToggleButton rubberBtn = new ToggleButton("Rubber");
+        ToggleButton lineBtn = new ToggleButton("Line");
+        ToggleButton rectBtn = new ToggleButton("Rectangle");
+        ToggleButton circleBtn = new ToggleButton("Circle");
+        ToggleButton ellipseBtn = new ToggleButton("Ellipse");
+        ToggleButton textBtn = new ToggleButton("Text");
+
+        ToggleButton[] toolsArr = { drawBtn, rubberBtn, lineBtn, rectBtn, circleBtn, ellipseBtn, textBtn };
+
+        ToggleGroup tools = new ToggleGroup();
+
+        for (ToggleButton tool : toolsArr) {
+            tool.setMinWidth(90);
+            tool.setToggleGroup(tools);
+            tool.setCursor(Cursor.HAND);
+        }
+
+        ColorPicker cpLine = new ColorPicker(Color.BLACK);
+        ColorPicker cpFill = new ColorPicker(Color.valueOf("#1688f0"));
+
+        TextArea text = new TextArea();
+        text.setPrefRowCount(1);
+
+        Slider slider = new Slider(1, 50, 3);
+        slider.setShowTickLabels(true);
+        slider.setShowTickMarks(true);
+
+        Label lineColor = new Label("Line Color");
+        Label fillColor = new Label("Fill Color");
+        Label lineWidth = new Label("3.0");
+
+        Button fillBtn = new Button("Fill");
+        Button clearBtn = new Button("Clear");
+        Button undoBtn = new Button("Undo");
+        Button redoBtn = new Button("Redo");
+        Button saveBtn = new Button("Save");
+        Button openBtn = new Button("Open");
+        Button exitBtn = new Button("Exit");
+
+        Button[] basicArr = { fillBtn, undoBtn, redoBtn, clearBtn, saveBtn, openBtn, exitBtn };
+
+        for (Button btn : basicArr) {
+            btn.setMinWidth(90);
+            btn.setCursor(Cursor.HAND);
+            btn.setTextFill(Color.WHITE);
+            btn.setStyle("-fx-background-color: #1688f0;");
+        }
+        exitBtn.setStyle("-fx-background-color: #f11b71;");
+
+        VBox buttons = new VBox(10);
+        buttons.getChildren().addAll(drawBtn, rubberBtn, lineBtn, rectBtn, circleBtn, ellipseBtn,
+                textBtn, text, lineColor, cpLine, fillColor, cpFill, lineWidth, slider, fillBtn, clearBtn, undoBtn,
+                redoBtn, openBtn, saveBtn, exitBtn);
+        buttons.setPadding(new Insets(5));
+        buttons.setStyle("-fx-background-color: #999");
+        buttons.setPrefWidth(100);
+
+        // Draw Canvas
+        Canvas canvas = new Canvas(CANVAS_WIDTH, CANVAS_HEIGHT);
+        GraphicsContext gc;
+        gc = canvas.getGraphicsContext2D();
+        gc.setLineWidth(1);
+
+        Line line = new Line();
+        Rectangle rectangle = new Rectangle();
+        Circle circle = new Circle();
+        Ellipse ellipse = new Ellipse();
+
+        canvas.setOnMousePressed(e -> {
+            if (drawBtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.beginPath();
+                gc.lineTo(e.getX(), e.getY());
+            } else if (rubberBtn.isSelected()) {
+                double rubberWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - rubberWidth / 2, e.getY() - rubberWidth / 2, rubberWidth, rubberWidth);
+            } else if (lineBtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                line.setStartX(e.getX());
+                line.setStartY(e.getY());
+            } else if (rectBtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.setFill(cpFill.getValue());
+                rectangle.setX(e.getX());
+                rectangle.setY(e.getY());
+            } else if (circleBtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.setFill(cpFill.getValue());
+                circle.setCenterX(e.getX());
+                circle.setCenterY(e.getY());
+            } else if (ellipseBtn.isSelected()) {
+                gc.setStroke(cpLine.getValue());
+                gc.setFill(cpFill.getValue());
+                ellipse.setCenterX(e.getX());
+                ellipse.setCenterY(e.getY());
+            } else if (textBtn.isSelected()) {
+                gc.setLineWidth(1);
+                gc.setFont(Font.font(slider.getValue()));
+                gc.setStroke(cpLine.getValue());
+                gc.setFill(cpFill.getValue());
+                gc.fillText(text.getText(), e.getX(), e.getY());
+                gc.strokeText(text.getText(), e.getX(), e.getY());
+            }
+        });
+
+        canvas.setOnMouseDragged(e -> {
+            if (drawBtn.isSelected()) {
+                gc.lineTo(e.getX(), e.getY());
+                gc.stroke();
+            } else if (rubberBtn.isSelected()) {
+                double rubberWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - rubberWidth / 2, e.getY() - rubberWidth / 2, rubberWidth, rubberWidth);
+            }
+        });
+
+        canvas.setOnMouseReleased(e -> {
+            if (drawBtn.isSelected()) {
+                gc.lineTo(e.getX(), e.getY());
+                gc.stroke();
+                gc.closePath();
+            } else if (rubberBtn.isSelected()) {
+                double rubberWidth = gc.getLineWidth();
+                gc.clearRect(e.getX() - rubberWidth / 2, e.getY() - rubberWidth / 2, rubberWidth, rubberWidth);
+            } else if (lineBtn.isSelected()) {
+                line.setEndX(e.getX());
+                line.setEndY(e.getY());
+                gc.strokeLine(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY());
+
+                undoHistory.push(new Line(line.getStartX(), line.getStartY(), line.getEndX(), line.getEndY()));
+            } else if (rectBtn.isSelected()) {
+                rectangle.setWidth(Math.abs((e.getX() - rectangle.getX())));
+                rectangle.setHeight(Math.abs((e.getY() - rectangle.getY())));
+                // rect.setX((rect.getX() > e.getX()) ? e.getX(): rect.getX());
+                if (rectangle.getX() > e.getX()) {
+                    rectangle.setX(e.getX());
+                }
+                // rect.setY((rect.getY() > e.getY()) ? e.getY(): rect.getY());
+                if (rectangle.getY() > e.getY()) {
+                    rectangle.setY(e.getY());
+                }
+
+                gc.fillRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
+                gc.strokeRect(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight());
+
+                undoHistory.push(
+                        new Rectangle(rectangle.getX(), rectangle.getY(), rectangle.getWidth(), rectangle.getHeight()));
+
+            } else if (circleBtn.isSelected()) {
+                circle.setRadius(
+                        (Math.abs(e.getX() - circle.getCenterX()) + Math.abs(e.getY() - circle.getCenterY())) / 2);
+
+                if (circle.getCenterX() > e.getX()) {
+                    circle.setCenterX(e.getX());
+                }
+                if (circle.getCenterY() > e.getY()) {
+                    circle.setCenterY(e.getY());
+                }
+
+                gc.fillOval(circle.getCenterX(), circle.getCenterY(), circle.getRadius(), circle.getRadius());
+                gc.strokeOval(circle.getCenterX(), circle.getCenterY(), circle.getRadius(), circle.getRadius());
+
+                undoHistory.push(new Circle(circle.getCenterX(), circle.getCenterY(), circle.getRadius()));
+            } else if (ellipseBtn.isSelected()) {
+                ellipse.setRadiusX(Math.abs(e.getX() - ellipse.getCenterX()));
+                ellipse.setRadiusY(Math.abs(e.getY() - ellipse.getCenterY()));
+
+                if (ellipse.getCenterX() > e.getX()) {
+                    ellipse.setCenterX(e.getX());
+                }
+                if (ellipse.getCenterY() > e.getY()) {
+                    ellipse.setCenterY(e.getY());
+                }
+
+                gc.strokeOval(ellipse.getCenterX(), ellipse.getCenterY(), ellipse.getRadiusX(), ellipse.getRadiusY());
+                gc.fillOval(ellipse.getCenterX(), ellipse.getCenterY(), ellipse.getRadiusX(), ellipse.getRadiusY());
+
+                undoHistory
+                        .push(new Ellipse(ellipse.getCenterX(), ellipse.getCenterY(), ellipse.getRadiusX(),
+                                ellipse.getRadiusY()));
+            }
+            redoHistory.clear();
+            Shape lastUndo = undoHistory.lastElement();
+            lastUndo.setFill(gc.getFill());
+            lastUndo.setStroke(gc.getStroke());
+            lastUndo.setStrokeWidth(gc.getLineWidth());
+
+        });
+        // color picker
+        cpLine.setOnAction(e -> {
+            gc.setStroke(cpLine.getValue());
+        });
+        cpFill.setOnAction(e -> {
+            gc.setFill(cpFill.getValue());
+        });
+
+        // slider
+        slider.valueProperty().addListener(e -> {
+            double width = slider.getValue();
+            if (textBtn.isSelected()) {
+                gc.setLineWidth(1);
+                gc.setFont(Font.font(slider.getValue()));
+                lineWidth.setText(String.format("%.1f", width));
+                return;
+            }
+            lineWidth.setText(String.format("%.1f", width));
+            gc.setLineWidth(width);
+        });
+
+        setupFill(fillBtn, gc, cpFill);
+        setupUndo(undoHistory, redoHistory, undoBtn, gc);
+        setupRedo(undoHistory, redoHistory, redoBtn, gc);
+        setupClear(undoHistory, redoHistory, clearBtn, gc);
+        setupOpen(primaryStage, openBtn, gc);
+        setupSave(primaryStage, saveBtn, canvas);
+        setupExit(primaryStage, exitBtn);
+
+        BorderPane pane = new BorderPane();
+        pane.setLeft(buttons);
+        pane.setCenter(canvas);
+
+        Scene scene = new Scene(pane, 1400, 760);
+
+        primaryStage.setTitle("Painter");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
     public static void main(String[] args) {
         launch(args);
     }
